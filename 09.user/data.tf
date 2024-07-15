10b80b544ffe359f8f71d3fb9c9a050e331cefb9
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-07-15 19:08:48 +0530

    updated for terraform

--- 09.user/data.tf
+++ 09.user/data.tf
@@ -21,8 +21,8 @@ data "aws_ami" "devops_ami" {
     values = ["hvm"]
   }
 }
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
+data "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project_name}/${var.environment}/user_sg_id"
 }
 data "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
