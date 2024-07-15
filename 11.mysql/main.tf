dc1e85b273e6aaa03dc1ff6c05f4c201ac56d121
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-07-15 23:29:25 +0530

    updated for terraform

--- 11.mysql/main.tf
+++ 11.mysql/main.tf
@@ -19,7 +19,7 @@
 module "mysql_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   ami = data.aws_ami.devops_ami.id
-  instance_type = "t3.medium"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.mysql_sg_id.value]
   subnet_id = local.db_subnet_id
   user_data = file("mysql.sh")
